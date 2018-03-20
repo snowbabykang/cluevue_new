66a5688fd23127fa4d04b4f38e059da4c252d3cd
121107684 <baiyangby@163.com>
2018-03-21 00:44:11 +0800

    third

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
 import ElementUI from 'element-ui'
+import Vuex from 'vuex'
+import axios from 'axios'
 import publicnav from './components/publicnav'
 import menuslider from './components/menu'
 import 'element-ui/lib/theme-chalk/index.css'
@@ -12,15 +15,28 @@ import './assets/css/bootstrap.css'
 import './assets/css/signin.css'
 import './assets/css/style.css'
 
+ 
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  // 定义状态
+  state: {
+    userToken: ''
+  }
+})
 Vue.use(ElementUI);
-Vue.component('public-nav',publicnav);
-Vue.component('menuslider',menuslider);
+axios.defaults.baseURL = 'http://clue.api.test';
+Vue.prototype.$ajax = axios
+
+//Vue.prototype.$ajax = axios
+Vue.component('public-nav', publicnav);
+Vue.component('menuslider', menuslider);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: {App,publicnav,menuslider},
+  components: { App, publicnav, menuslider },
   template: '<App/>'
 })
