f02699a30c83e40cd22022e7d7aed306e6176323
baiyang <baiyang@bang-20160328sg.hxsd.local>
2018-03-30 17:00:13 +0800

    去请求

--- src/views/clue/cluelist.vue
+++ src/views/clue/cluelist.vue
@@ -25,7 +25,7 @@
 							<div class="block pull-left" v-if="showmore">
 									<label>请选择时间</label>
 									<el-date-picker
-									v-model="value7"
+									v-model="datatime"
 									type="daterange"
 									align="right"
 									unlink-panels
@@ -37,18 +37,7 @@
 							</div>
 							<div class="block pull-left ml-lg"  v-if="showmore">
 								<label>线索来源</label>
-								<el-select v-model="source" clearable placeholder="请选择线索来源">
-								<el-option
-									v-for="item in options"
-									:key="item.value"
-									:label="item.label"
-									:value="item.value">
-								</el-option>
-							</el-select>
-							</div>
-							 <div class="block pull-left ml-lg"  v-if="showmore">
-								<label>单位</label>
-								<el-select v-model="company" clearable placeholder="请选择单位">
+								<el-select v-model="postdata.source" clearable placeholder="请选择线索来源">
 								<el-option
 									v-for="item in options"
 									:key="item.value"
@@ -59,7 +48,7 @@
 							</div>
 							<div class="block pull-left ml-lg"  v-if="showmore">
 								<label>职位</label>
-								<el-select v-model="post" clearable placeholder="请选择职位">
+								<el-select v-model="postdata.post" clearable placeholder="请选择职位">
 								<el-option
 									v-for="item in options"
 									:key="item.value"
@@ -70,7 +59,7 @@
 							</div>
 							<div class="block pull-left ml-lg"  v-if="showmore">
 								<label>级别</label>
-								<el-select v-model="level" clearable placeholder="请选择级别">
+								<el-select v-model="postdata.level" clearable placeholder="请选择级别">
 								<el-option
 									v-for="item in options"
 									:key="item.value"
@@ -81,7 +70,7 @@
 							</div>
 							<div class="block pull-left ml-lg"  v-if="showmore">
 								<label>单位</label>
-								<el-select v-model="company" clearable placeholder="请选择">
+								<el-select v-model="postdata.company" clearable placeholder="请选择">
 									<el-option
 										v-for="item in options"
 										:key="item.value"
@@ -92,7 +81,7 @@
 							</div>
 							<div class="block pull-left ml-lg form-group">
 
-								<el-input v-model="keyword" placeholder="输入线索来源"></el-input>
+								<el-input v-model="postdata.keyword" placeholder="输入线索来源"></el-input>
 							</div>
 							<div class="block pull-left ml-lg">
 									<el-button type="primary" @click="searchdata" style="margin-left:15px">查询</el-button>
@@ -110,14 +99,12 @@
 				<div style="padding:15px">
 						<el-tabs type="card">
 						<el-tab-pane label="问题线索">
-							<el-table :data="tableData" border show-summary style="width: 100%">
+							<el-table :data="tableData" border style="width: 100%" v-on:sort-change="tablecha">
 								<el-table-column
 									type="index"
 									label="序号"
-									:index="indexMethod"
 									width="60">
 									</el-table-column>
-								
 								<el-table-column
 								prop="source"
 								label="线索来源">
@@ -129,22 +116,22 @@
 								</el-table-column>
 								<el-table-column
 								prop="reflected_name"
-								sortable
+								sortable='custom'
 								label="被反映人">
 								</el-table-column>
 								<el-table-column
 								prop="company"
-								sortable
+								sortable='custom'
 								label="单位">
 								</el-table-column>
 								<el-table-column
 								prop="post"
-								sortable
+								sortable='custom'
 								label="职位">
 								</el-table-column>
 								<el-table-column
 								prop="level"
-								sortable
+								sortable='custom'
 								label="级别">
 								</el-table-column>
 								<el-table-column
@@ -153,11 +140,20 @@
 									width="100">
 									<template slot-scope="scope">
 										<el-tag
-										:type="scope.row.tag === '家' ? 'primary' : 'success'"
-										close-transition>{{scope.row.tag}}</el-tag>
+										:type="scope.row.clue_state == '0' ? 'primary' : 'success'"
+										close-transition>{{scope.row.clue_state=='0'?'以办':'未办'}}</el-tag>
 									</template>
 									</el-table-column>
 							</el-table>
+							<el-pagination
+								@size-change="handleSizeChange"
+								@current-change="handleCurrentChange"
+								:current-page="current_page"
+								:page-sizes="[1, 5, 10, 20]"
+								:page-size="100"
+								layout="total, sizes, prev, pager, next, jumper"
+								:total="totaldata">
+							</el-pagination>
 						</el-tab-pane>
 						<el-tab-pane label="案件管理">
 							<el-tabs tab-position="left" style="height:600px;">
@@ -186,8 +182,19 @@ export default {
 	data() {
 		return {
 			cluefrom: "",
-			keyword:'',
+			postdata:{
+				keyword:'zhangsan',
+				source:'',
+				company:'',
+				post:'',
+				level:'',
+				company:'',
+				orders:{},
+				index:1,
+				size:1
+			},
 			tableData: [],
+			datatime:'',
 			options: [
 				{
 					value: "选项1",
@@ -210,10 +217,9 @@ export default {
 					label: "北京烤鸭"
 				}
 			],
-			value4: "",
-
-			value7: "",
-			showmore:false
+			showmore:false,
+			current_page:1,
+			totaldata:1
 		};
 	},
 	computed: {
@@ -222,31 +228,51 @@ export default {
 		}
 	},
 	methods: {
+		tablecha:function(e){
+			console.log(e,1111)
+			this.postdata.orders.column = e.prop
+			this.postdata.orders.order = e.order=="descending"?'0':'1'
+			this.getdata()
+		},
 		searchdata: function() {
-			console.log(this.cluefrom);
+			this.getdata()
 		},
 		getlist: function(data = {}) {
 		 // this.$ajax.post("").then;
 		},
 		topsearch:function(){
 			this.showmore = !this.showmore;
+		},
+		handleSizeChange(val) {
+			console.log(`每页 ${val} 条`);
+			this.postdata.size = val
+			this.getdata()
+
+		},
+		handleCurrentChange(val) {
+			console.log(`当前页: ${val}`);
+			++this.postdata.index;
+			this.getdata()
+		},
+		getdata(){
+			this.$ajax.post('/api/clue/clue_keyword_search',this.postdata).then((res)=>{
+				this.tableData = res.data.data;
+				this.current_page = res.data.current_page;
+				this.totaldata = res.data.total;
+			})
 		}
-		
 	},
 	watch:{
-			value7:function(n,o){
-				console.log(n,o)
-			}
-		},
+		value7:function(n,o){
+			console.log(n,o)
+		}
+	},
 	// created(){
 	//    //this.getlist()
 
 	// },
 	mounted(){
-		var data = {keyword:"zhangsan"}
-		 this.$ajax.post('/api/clue/clue_keyword_search',data).then((res)=>{
-			this.tableData = res.data.data
-		})
+		this.getdata()
 	}
 };
 </script>
